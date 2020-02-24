0bce316be605aba52b7520f5d0dbd42099764128
dorjepadma <dorjepadma@gmail.com>
2020-02-23 19:49:07 -0800

    bringing in the work

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import request from 'superagent';
 import Beer from './beer-item.js';
-
+import './App.css';
+import { 
+  Route, 
+  Switch,
+  Link,
+  BrowserRouter as Router, 
+} from 'react-router-dom'; 
 export default class App extends Component {
     state = { beer: [] }
-        
-    async componentDidMount() {
-        const data = await request.get(`http://agile-coast-09251.herokuapp.com/api/beers`)
 
+    async componentDidMount() {
+        const data = await request.get(`https://agile-coast-09251.herokuapp.com/api/beers`)
         this.setState({ beer: data.body})
     }
-
     render() {
-        
         return (
-            
                 <ul>
                     {
-                    this.state.beer.map(beer =>
-                    <Beer beer={beer} /> )
+                    this.state.beer.map((beer, index) =>
+                    <Beer beer={beer} key={index} /> )
                     }   
                 </ul>
-            
             )
     }
 }
